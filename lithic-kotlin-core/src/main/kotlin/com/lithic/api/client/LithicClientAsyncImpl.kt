62e28395b41af99f7c223bbb9830e957be85560b
meorphis <meorphis@gmail.com>
2024-03-14 16:11:08 -0400

    chore: add back removed code
    This reverts commit 7796e44d8c6d8cc635710c1998c522c7975ed5eb.

--- lithic-kotlin-core/src/main/kotlin/com/lithic/api/client/LithicClientAsyncImpl.kt
+++ lithic-kotlin-core/src/main/kotlin/com/lithic/api/client/LithicClientAsyncImpl.kt
@@ -69,6 +69,8 @@ constructor(
         ResponderEndpointServiceAsyncImpl(clientOptions)
     }
 
+    private val webhooks: WebhookServiceAsync by lazy { WebhookServiceAsyncImpl(clientOptions) }
+
     private val externalBankAccounts: ExternalBankAccountServiceAsync by lazy {
         ExternalBankAccountServiceAsyncImpl(clientOptions)
     }
@@ -123,6 +125,8 @@ constructor(
     override fun transactions(): TransactionServiceAsync = transactions
 
     override fun responderEndpoints(): ResponderEndpointServiceAsync = responderEndpoints
+
+    override fun webhooks(): WebhookServiceAsync = webhooks
 
     override fun externalBankAccounts(): ExternalBankAccountServiceAsync = externalBankAccounts
 
